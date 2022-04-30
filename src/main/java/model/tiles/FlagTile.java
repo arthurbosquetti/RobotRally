c7887bc9e8ce46205034f6caf7659360be16c382
margretsnorradottir <margretsnorradottir@127.0.0.1>
2022-04-30 12:50:06 +0200

    flagyflag

--- src/main/java/model/tiles/FlagTile.java
+++ src/main/java/model/tiles/FlagTile.java
@@ -27,6 +27,7 @@ public class FlagTile extends Tile {
             robot.setFlag1(true);
         } else if (flagNum == 2 && robot.getFlag1()) {
             robot.setFlag2(true);
+            robot.setWinner(true);
         }
     }
 
