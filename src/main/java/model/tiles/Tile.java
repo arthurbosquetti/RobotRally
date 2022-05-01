689b8f1303839244b59a740b105921f44dc88d71
Simon <simonhermansen5dk@gmail.com>
2022-05-01 13:14:58 +0200

    Fixed merge conflicts?

--- src/main/java/model/tiles/Tile.java
+++ src/main/java/model/tiles/Tile.java
@@ -1,7 +1,5 @@
 package model.tiles;
 
-
-<<<<<<< HEAD
 import model.Robot;
 import view.TileType;
 
@@ -10,14 +8,6 @@ public interface Tile {
 	void setRobotOn(Robot robot);
 
 	void setRobotOff() ;
-=======
-import view.TileType;
-
-import controller.Game;
-import model.AI;
-import model.Board;
-import model.Robot;
->>>>>>> 549a83ac096c7bc44a8357efd9ebbb1822251ecc
 
 	Robot getRobotOn() ;
 
